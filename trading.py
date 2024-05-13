bababfc44b67ec8f216b4ac95fd5f4844ec668dd
Pratik301002 <159586077+Pratik301002@users.noreply.github.com>
2024-05-13 14:02:15 +0530

    Update trading.py

--- trading.py
+++ trading.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
 import pickle
 
 pickle_in = open(r"C:\Users\Pratik\Downloads\model (1).pkl", 'rb')
